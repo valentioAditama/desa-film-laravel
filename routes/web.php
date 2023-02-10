3993a8cda5cdf9ca34211d5e7c4b0d76be0445df
valentioaditama <valentioaditama8@gmail.com>
2023-02-10 21:24:24 +0700

    update fixing

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,9 @@ use Illuminate\Support\Facades\Storage;
 |
 */
 
-Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', function () {
+    return view('home');
+});
 
 Auth::routes();
 
