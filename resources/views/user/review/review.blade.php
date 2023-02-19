d2aef87ba0d0efe80b73a788fcd6b5662ed806dd
valentioaditama <valentioaditama8@gmail.com>
2023-02-19 22:24:06 +0700

    update

--- resources/views/user/review/review.blade.php
+++ resources/views/user/review/review.blade.php
@@ -1,89 +1,89 @@
 @extends('layouts.app')
 @include('components.navbar.navbarUser')
 <!-- Banner image -->
-<div class="p-5 text-center bg-image" style="
+<div class="p-5 text-center bg-image img-fluid" style="
       background-image: url(/storage/banner/{{$data->banner}});
       height: 400px;
     ">
-    <div class="mask" style="background-color: rgba(0, 0, 0, 0.6);">
-        <div class="d-flex justify-content-center align-items-center h-100">
-            <div class="text-white">
-                <h1 class="mb-3">{{$data->title}}</h1>
-                <h4 class="mb-3">
-                    {{$data->category}}
-                </h4>
-            </div>
-        </div>
+  <div class="mask" style="background-color: rgba(0, 0, 0, 0.6);">
+    <div class="d-flex justify-content-center align-items-center h-100">
+      <div class="text-white">
+        <h1 class="mb-3">{{$data->title}}</h1>
+        <h4 class="mb-3">
+          {{$data->category}}
+        </h4>
+      </div>
     </div>
+  </div>
 </div>
 
 <div class="container contentReview">
-    <!-- content -->
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="mb-3">
-                <h3>{{$data->title}}</h3>
-                <h6>{{$data->category}}</h6>
-            </div>
-            <div class="mb-3">
-                <img src="/storage/banner/{{$data->banner}}" class="img-fluid bannerReview" alt="">
-            </div>
-            <div class="mb-3">
-                <div class="row">
-                    <div class="col-md-6">
-                        <img src="/storage/poster/{{$data->poster}}" class="img-fluid posterReview" alt="">
-                    </div>
-                    <div class="col-md-6">
-                        <p>{{$data->description}}</p>
-                    </div>
-                    <iframe src="{{$data->link_trailer}}" class="trailerReview mt-3" frameborder="0"></iframe>
-                </div>
-                <a href="{{$data->link_film}}" class="btn btn-dark btn-lg mt-3">Watch Movie</a>
-            </div>
-            <div class="mt-5">
-                <!-- comment content -->
-                @guest
-                @if (Route::has('login'))
-                <!--  -->
-                @endif
-                @else
-                <h4>Rating Star</h4>
-                <form action="/review/feedback" method="post">
-                    <div class="form-check form-check-inline">
-                        <input class="form-check-input" type="radio" name="starReview" id="inlineRadio1" value="1" />
-                        <label class="form-check-label" for="inlineRadio1">Star 1</label>
-                    </div>
-
-                    <div class="form-check form-check-inline">
-                        <input class="form-check-input" type="radio" name="starReview" id="inlineRadio2" value="2" />
-                        <label class="form-check-label" for="inlineRadio2">Star 2</label>
-                    </div>
-
-                    <div class="form-check form-check-inline">
-                        <input class="form-check-input" type="radio" name="starReview" id="inlineRadio2" value="3" />
-                        <label class="form-check-label" for="inlineRadio2">Star 3</label>
-                    </div>
-
-                    <div class="form-check form-check-inline">
-                        <input class="form-check-input" type="radio" name="starReview" id="inlineRadio2" value="4" />
-                        <label class="form-check-label" for="inlineRadio2">Star 4</label>
-                    </div>
-
-                    <div class="form-check form-check-inline">
-                        <input class="form-check-input" type="radio" name="starReview" id="inlineRadio2" value="5" />
-                        <label class="form-check-label" for="inlineRadio2">Star 5</label>
-                    </div>
-
-                    <div class="mt-3">
-                        <textarea name="preview" id="" cols="30" rows="10" class="form-control" placeholder="Add Description"></textarea>
-                    </div>
-
-                    <div class="mt-3">
-                        <button type="submit" class="btn btn-primary btn-sm">Give feedback</button>
-                    </div>
-                </form>
-                @endguest
-            </div>
+  <!-- content -->
+  <div class="row justify-content-center">
+    <div class="col-md-8">
+      <div class="mb-3">
+        <h3>{{$data->title}}</h3>
+        <h6>{{$data->category}}</h6>
+      </div>
+      <div class="mb-3">
+        <img src="/storage/banner/{{$data->banner}}" class="img-fluid bannerReview" alt="">
+      </div>
+      <div class="mb-3">
+        <div class="row d-flex">
+          <div class="col-md-4 justify-content-center">
+            <img src="/storage/poster/{{$data->poster}}" class="img-fluid posterReview" alt="">
+          </div>
+          <div class="col-md-8 justify-content-center">
+            <p>{{$data->description}}</p>
+          </div>
+          <iframe src="{{$data->link_trailer}}" class="trailerReview mt-3" frameborder="0"></iframe>
         </div>
+        <a href="{{$data->link_film}}" class="btn btn-dark btn-lg mt-3">Watch Movie</a>
+      </div>
+      <div class="mt-5">
+        <!-- comment content -->
+        @guest
+        @if (Route::has('login'))
+        <!--  -->
+        @endif
+        @else
+        <h4>Rating Star</h4>
+        <form action="/review/feedback" method="post">
+          <div class="form-check form-check-inline">
+            <input class="form-check-input" type="radio" name="starReview" id="inlineRadio1" value="1" />
+            <label class="form-check-label" for="inlineRadio1">Star 1</label>
+          </div>
+
+          <div class="form-check form-check-inline">
+            <input class="form-check-input" type="radio" name="starReview" id="inlineRadio2" value="2" />
+            <label class="form-check-label" for="inlineRadio2">Star 2</label>
+          </div>
+
+          <div class="form-check form-check-inline">
+            <input class="form-check-input" type="radio" name="starReview" id="inlineRadio2" value="3" />
+            <label class="form-check-label" for="inlineRadio2">Star 3</label>
+          </div>
+
+          <div class="form-check form-check-inline">
+            <input class="form-check-input" type="radio" name="starReview" id="inlineRadio2" value="4" />
+            <label class="form-check-label" for="inlineRadio2">Star 4</label>
+          </div>
+
+          <div class="form-check form-check-inline">
+            <input class="form-check-input" type="radio" name="starReview" id="inlineRadio2" value="5" />
+            <label class="form-check-label" for="inlineRadio2">Star 5</label>
+          </div>
+
+          <div class="mt-3">
+            <textarea name="preview" id="" cols="30" rows="10" class="form-control" placeholder="Add Description"></textarea>
+          </div>
+
+          <div class="mt-3">
+            <button type="submit" class="btn btn-primary btn-sm">Give feedback</button>
+          </div>
+        </form>
+        @endguest
+      </div>
     </div>
+  </div>
 </div>
